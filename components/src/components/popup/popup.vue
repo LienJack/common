12f09943292736930fbdb4ae9c7d241f837a9dbc
blacksnow233 <longyuhuhao@126.com>
2018-09-26 17:37:36 +0800

    add popup

---
+++ components/src/components/popup/popup.vue
@@ -0,0 +1,29 @@
+<template>
+  <transition :name="currentTransition">
+    <div v-show="value" class="xf-popup" :class="[position ? 'xf-popup-' + position : '']">
+      <slot/>
+    </div>
+  </transition>
+</template>
+<script>
+export default {
+  props: {
+    value: {
+      type: Boolean,
+      default: true,
+    },
+    popupTransition: {
+      type: String,
+      default: 'popup'
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+  $prefix: "xf-popup";
+   .#{$prefix} {
+     
+   }
+</style>
+
+
